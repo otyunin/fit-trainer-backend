a29b0e36e5aaf27f7a08291b5f1446f8f46fcd55
Oleg Tyunin <otyunin06@gmail.com>
2018-10-30 00:38:41 +0200

    Password hashing and saving to db

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,8 +1,32 @@
+const User = require('../models/user')
+
 module.exports = {
   signIn: async (req, res) => {
     res.send('Sign in called')
   },
   signUp: async (req, res) => {
-    res.send('Sign up called')
+    const { email, password, repeatPassword } = req.body
+
+    // Check if there is a user with the same email
+    const foundUser = await User.findOne({ email })
+    if (foundUser) {
+      return res.status(400).json({
+        success: false,
+        message: 'Email is already in use'
+      })
+    }
+    // Check the passwords match
+    if (password !== repeatPassword) {
+      return res.status(400).json({
+        success: false,
+        message: 'Passwords don\'t match'
+      })
+    }
+
+    // Create a new user
+    const newUser = new User({ email, password })
+    await newUser.save()
+
+    res.status(201).json({ success: true })
   }
 }
--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt-nodejs')
 const Schema = mongoose.Schema
+
+const SALT_ROUNDS = 10
 
 const UserSchema = new Schema({
   email: {
@@ -20,6 +23,29 @@ const UserSchema = new Schema({
     default: Date.now
   }
 })
+
+UserSchema.pre('save', async function (next) {
+  const user = this
+  try {
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next()
+    // Generate a salt
+    const salt = await bcrypt.genSaltSync(SALT_ROUNDS)
+    // Generate a password hash and re-assign hashed version over original, plain text password
+    this.password = await bcrypt.hashSync(this.password, salt)
+    next()
+  } catch (error) {
+    next(error)
+  }
+})
+
+UserSchema.methods.isValidPassword = async function (newPassword) {
+  try {
+    return await bcrypt.compareSync(newPassword, this.password)
+  } catch (error) {
+    throw new Error(error)
+  }
+}
 
 const User = mongoose.model('User', UserSchema)
 
